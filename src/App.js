a860e20ff823682a7fdf23f79b19fe736d8a4ab3
Kishore Kumar <kishorejaipal477@gmail.com>
2023-05-25 04:01:36 +0000

    simple calender code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import React, {useState} from 'react'
+import moment from 'moment'
+
+function App() {
+
+  const [dateState, setDateState] = useState(new Date())
+  const changeDate = (e) => {
+    setDateState(e)
+  }
+  return (
+    <div>
+      <Calendar  className="App" value={dateState}
+      onChange={changeDate}/>
+       <p>Current selected date is <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
+    </div>
+  );
+}
+
+export default App;
